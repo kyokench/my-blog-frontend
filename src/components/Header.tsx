bbb3ba16a708b0a9288ae0947953ef748ccd13b5
kyoken ch <kyokench@gmail.com>
2025-07-08 08:28:39 +0900

    comitt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-green-500 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">
           <Link href="/">私のブログ</Link>
