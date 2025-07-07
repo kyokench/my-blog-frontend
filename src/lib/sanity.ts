b2410efa2a2e1b5ae19dd1572760e7d0740099b0
kyokench <kyokench@gmail.com>
2025-07-07 21:06:39 +0900

    fix: update sanity apiVersion to current date

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: 'wjvd5gs5',
   dataset: 'production',
-  apiVersion: '2025-07-07', // 今日の日付でOK
+  apiVersion: '2023-01-01',
   useCdn: true,
 })
